e3e99559a82335677f3a474be000cb1a84b4a8cf
Jose Cabrera <murdoc.jose.6@gmail.com>
2024-10-03 17:08:29 -0300

    Before fixing makefile

---
+++ src/Game.cpp
@@ -0,0 +1,42 @@
+#include "Game.h"
+#include <iostream>
+
+Game::Game()
+{
+    std::cout << "Game constructor" << std::endl;
+}
+
+Game::~Game()
+{
+    std::cout << "Game destructor" << std::endl;
+}
+
+void Game::Initialize()
+{
+    // ...
+}
+
+void Game::Run()
+{
+    // ...
+}
+
+void Game::ProcessInput()
+{
+    // ...
+}
+
+void Game::Update()
+{
+    // ...
+}
+
+void Game::Render()
+{
+    // ...
+}
+
+void Game::Destroy()
+{
+    // ...
+}
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,20 +1,16 @@
-#include <iostream>
 #include <SDL.h>
-#include <SDL_image.h>
-#include <SDL_ttf.h>
-#include <SDL_mixer.h>
-#include <glm/glm.hpp>
-#include <imgui/imgui.h>
-#include <sol/sol.hpp>
+#include "Game.h"
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
-    sol::state lua;
-    lua.open_libraries(sol::lib::base);
-    SDL_Init(SDL_INIT_EVERYTHING);
+    std::cout << "Ending main" << std::endl; // Línea de prueba
 
-    glm::vec2 velocity = glm::vec2(1.0f, 0.0f);
+    Game game;
 
-    std::cout << "Hello, World!" << std::endl;
+    game.Initialize();
+    game.Run();
+    game.Destroy();
+
     return 0;
 }
