17057a835d8ad64ae5590031bb462996faffdf9b
Jose Cabrera <murdoc.jose.6@gmail.com>
2024-10-03 15:35:54 -0300

    [FEAT] Added main function boilerplate

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,17 +3,18 @@
 #include <SDL_image.h>
 #include <SDL_ttf.h>
 #include <SDL_mixer.h>
+#include <glm/glm.hpp>
+#include <imgui/imgui.h>
+#include <sol/sol.hpp>
 
 int main(int argc, char *argv[])
 {
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
-    {
-        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
-        return 1;
-    }
+    sol::state lua;
+    lua.open_libraries(sol::lib::base);
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    glm::vec2 velocity = glm::vec2(1.0f, 0.0f);
 
     std::cout << "Hello, World!" << std::endl;
-
-    SDL_Quit();
     return 0;
 }
