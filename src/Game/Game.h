74c73387f7ea0199320638fc10984530c71a260e
Jose Cabrera <murdoc.jose.6@gmail.com>
2024-12-17 11:20:13 -0300

    [FEATURE] Implement AssetStore for managing textures and update SpriteComponent to include assetId

--- src/Game/Game.h
+++ src/Game/Game.h
@@ -3,6 +3,7 @@
 
 #include <SDL.h>
 #include "../ECS/ECS.h"
+#include "../AssetStore/AssetStore.h"
 
 const int FPS = 30;
 const int MILISECONDS_PER_FRAME = 1000 / FPS;
@@ -16,6 +17,7 @@ private:
     SDL_Renderer *renderer;
 
     std::unique_ptr<Registry> registry;
+    std::unique_ptr<AssetStore> assetStore;
 
 public:
     Game();
