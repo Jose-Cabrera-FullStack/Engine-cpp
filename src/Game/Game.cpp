d616980e15b57d15daa2b8dc689a98756853133e
Jose Cabrera <murdoc.jose.6@gmail.com>
2024-12-20 10:52:05 -0300

    [REFINE] Correct sprite zIndex parameters for tank and truck entities and optimize rendering order in RenderSystem

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -142,12 +142,12 @@ void Game::LoadLevel(int level)
     Entity tank = registry->CreateEntity();
     tank.AddComponent<TransformComponent>(glm::vec2(10.0, 10.0), glm::vec2(1.0, 1.0), 0.0);
     tank.AddComponent<RigidBodyComponent>(glm::vec2(40.0, 0.0));
-    tank.AddComponent<SpriteComponent>("tank-image", 2, 32, 32);
+    tank.AddComponent<SpriteComponent>("tank-image", 32, 32, 2);
 
     Entity truck = registry->CreateEntity();
     truck.AddComponent<TransformComponent>(glm::vec2(50.0, 100.0), glm::vec2(1.0, 1.0), 0.0);
     truck.AddComponent<RigidBodyComponent>(glm::vec2(0.0, 50.0));
-    truck.AddComponent<SpriteComponent>("truck-image", 1, 32, 32);
+    truck.AddComponent<SpriteComponent>("truck-image", 32, 32, 1);
 }
 
 void Game::Setup()
--- src/Systems/RenderSystem.h
+++ src/Systems/RenderSystem.h
@@ -6,6 +6,7 @@
 #include "../Components/SpriteComponent.h"
 #include "../AssetStore/AssetStore.h"
 #include <SDL.h>
+#include <algorithm>
 
 class RenderSystem : public System
 {
@@ -18,11 +19,33 @@ public:
 
     void Update(SDL_Renderer *renderer, std::unique_ptr<AssetStore> &assetStore)
     {
-        // Loop all entities that the system is interested in
+        // Create a vector with both Sprint and Transform components of all entities
+        struct RenderableEntity
+        {
+            SpriteComponent spriteComponent;
+            TransformComponent transformComponent;
+        };
+
+        std::vector<RenderableEntity> renderableEntities;
         for (auto entity : GetSystemEntities())
         {
-            const auto transform = entity.GetComponent<TransformComponent>();
-            const auto sprite = entity.GetComponent<SpriteComponent>();
+            RenderableEntity renderableEntity;
+            renderableEntity.spriteComponent = entity.GetComponent<SpriteComponent>();
+            renderableEntity.transformComponent = entity.GetComponent<TransformComponent>();
+            renderableEntities.emplace_back(renderableEntity);
+        }
+
+        std::sort(
+            renderableEntities.begin(),
+            renderableEntities.end(),
+            [](const RenderableEntity &a, const RenderableEntity &b)
+            { return a.spriteComponent.zIndex < b.spriteComponent.zIndex; });
+
+        // Loop all entities that the system is interested in
+        for (auto entity : renderableEntities)
+        {
+            const auto transform = entity.transformComponent;
+            const auto sprite = entity.spriteComponent;
 
             // Set the source rectangle of our original sprite texture
             SDL_Rect srcRect = sprite.srcRect;
