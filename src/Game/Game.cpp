389ce6e59078b058a04b9b6d4082b4c3e5efe5e9
Jose Cabrera <murdoc.jose.6@gmail.com>
2025-01-05 11:03:25 -0300

    [FIX] Update ImGui SDL implementation and restore event processing

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -29,7 +29,7 @@
 #include <glm.hpp>
 #include <imgui.h>
 #include <imgui_sdl.h>
-// #include <imgui_impl_sdl2.h>
+#include <imgui_impl_sdl.h>
 #include <fstream>
 
 int Game::windowWidth;
@@ -106,15 +106,14 @@ void Game::ProcessInput()
     SDL_Event sdlEvent;
     while (SDL_PollEvent(&sdlEvent))
     {
-        // ImGui_ImplSDL2_ProcessEvent(&sdlEvent);
-        // ImGuiIO &io = ImGui::GetIO();
+        ImGui_ImplSDL2_ProcessEvent(&sdlEvent);
+        ImGuiIO &io = ImGui::GetIO();
+        int mouseX, mouseY;
+        const int buttons = SDL_GetMouseState(&mouseX, &mouseY);
 
-        // int mouseX, mouseY;
-        // const int buttons = SDL_GetMouseState(&mouseX, &mouseY);
-
-        // io.MousePos = ImVec2(mouseX, mouseY);
-        // io.MouseDown[0] = buttons & SDL_BUTTON(SDL_BUTTON_LEFT);
-        // io.MouseDown[1] = buttons & SDL_BUTTON(SDL_BUTTON_RIGHT);
+        io.MousePos = ImVec2(mouseX, mouseY);
+        io.MouseDown[0] = buttons & SDL_BUTTON(SDL_BUTTON_LEFT);
+        io.MouseDown[1] = buttons & SDL_BUTTON(SDL_BUTTON_RIGHT);
 
         switch (sdlEvent.type)
         {
