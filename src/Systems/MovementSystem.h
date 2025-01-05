95db56b01b847861f83f4cb91367cf69915c1a03
Jose Cabrera <murdoc.jose.6@gmail.com>
2025-01-05 16:14:50 -0300

    [FIX] Prevent player movement outside map boundaries and kill off-screen entities

--- src/Systems/MovementSystem.h
+++ src/Systems/MovementSystem.h
@@ -23,6 +23,30 @@ public:
 
             transform.position.x += rigidBody.velocity.x * deltaTime;
             transform.position.y += rigidBody.velocity.y * deltaTime;
+
+            // Prevent the main player from moving outside the map boundaries
+            if (entity.HasTag("player"))
+            {
+                int paddingLeft = 10;
+                int paddingTop = 10;
+                int paddingRight = 50;
+                int paddingBottom = 50;
+                transform.position.x = transform.position.x < paddingLeft ? paddingLeft : transform.position.x;
+                transform.position.x = transform.position.x > Game::mapWidth - paddingRight ? Game::mapWidth - paddingRight : transform.position.x;
+                transform.position.y = transform.position.y < paddingTop ? paddingTop : transform.position.y;
+                transform.position.y = transform.position.y > Game::mapHeight - paddingBottom ? Game::mapHeight - paddingBottom : transform.position.y;
+            }
+
+            bool isEntityOutsideMap = (transform.position.x < 0 ||
+                                       transform.position.x > Game::mapWidth ||
+                                       transform.position.y < 0 ||
+                                       transform.position.y > Game::mapHeight);
+
+            // Kill all entities that move outside the map boundaries
+            if (isEntityOutsideMap && !entity.HasTag("player"))
+            {
+                entity.Kill();
+            }
         }
     };
 };
